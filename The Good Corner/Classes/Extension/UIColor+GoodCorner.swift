66c955fb654bdd1bb73139291fc60e08077e905d
Olivier Duménil <olivier.dumenil@epitech.eu>
2020-07-08 21:55:12 +0200

    add color palette

---
+++ The Good Corner/Classes/Extension/UIColor+GoodCorner.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+GoodCorner.swift
+//  The Good Corner
+//
+//  Created by Olivier Duménil on 08/07/2020.
+//  Copyright © 2020 Olivier Duménil. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+    static let background =					UIColor(named: "background")!
+    static let primary =					UIColor(named: "primary")!
+    static let secondary =					UIColor(named: "secondary")!
+    static let tertiary =					UIColor(named: "tertiary")!
+    static let semiTransparentBackground =	UIColor(named: "semiTransparentBackground")!
+    static let textPrimary =				UIColor(named: "textPrimary")!
+
+}
