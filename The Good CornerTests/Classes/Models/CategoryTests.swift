1cc33cd5060b24f937ed21f183be80a49c4b22f6
Olivier Duménil <olivier.dumenil@epitech.eu>
2020-07-09 20:50:03 +0200

    add Category tests

--- The Good CornerTests/Classes/Models/CategoryTests.swift
+++ The Good CornerTests/Classes/Models/CategoryTests.swift
@@ -12,12 +12,42 @@ import XCTest
 
 class CategoryTests: XCTestCase {
 
+    let categories: [The_Good_Corner.Category] = [.init(id: 1, name: "test"), .init(id: 2, name: "test")]
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testGettingExistingCategory() {
+        // given
+
+        // when
+        let category = categories.categoryWithId(1)
+        // then
+        XCTAssert(category == categories.first)
+    }
+
+    func testGettingMissingCategory() {
+        // given
+
+        // when
+        let category = categories.categoryWithId(3)
+        // then
+        XCTAssert(category == .unknown)
+    }
+
+    func testHashable() {
+        // given
+        let category = categories.first!
+        var set = Set([category])
+        // when
+        set.insert(category)
+        // then
+        XCTAssert(set.count == 1)
     }
 
 }
