bae053ce24d858635614d771138d6d2f216200f7
Olivier Duménil <olivier.dumenil@epitech.eu>
2020-07-09 17:44:41 +0200

    add base classes for tests

---
+++ The Good CornerTests/Classes/Screen/AdsList/AdsListViewModelTests.swift
@@ -0,0 +1,22 @@
+//
+//  AdsListViewModelTests.swift
+//  The Good CornerTests
+//
+//  Created by Olivier Duménil on 09/07/2020.
+//  Copyright © 2020 Olivier Duménil. All rights reserved.
+//
+
+import XCTest
+@testable import The_Good_Corner
+
+class AdsListViewModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
