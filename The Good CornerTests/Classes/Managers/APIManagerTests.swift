7ada3693aed1736f02665cde63637ff20225100d
Olivier Duménil <olivier.dumenil@epitech.eu>
2020-07-10 10:08:03 +0200

    add APIManager tests

--- The Good CornerTests/Classes/Managers/APIManagerTests.swift
+++ The Good CornerTests/Classes/Managers/APIManagerTests.swift
@@ -12,12 +12,61 @@ import XCTest
 
 class APIManagerTests: XCTestCase {
 
+    var api: APIManager!
+
+    var shouldError = false
+
+    var result: Result<UIImage, APIError>!
+
+    let url = URL(string: "https://raw.githubusercontent.com/leboncoin/paperclip/master/ad-thumb/2c9563bbe85f12a5dcaeb2c40989182463270404.jpg")!
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let requestSender = RequestSenderMock(handler: handleRequest(_:))
+        api = APIManager(requestSender: requestSender)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testThumbnailImageFetching() {
+        // given
+        shouldError = false
+        // when
+        api.getImage(url: url, completion: {
+            self.result = $0
+        })
+        // then
+        do {
+            let _ = try result.get()
+            XCTAssert(true)
+        } catch {
+            XCTAssert(false)
+        }
+    }
+
+    func testThumbnailImageFetchingInError() {
+        // given
+        shouldError = true
+        // when
+        api.getImage(url: url, completion: {
+            self.result = $0
+        })
+        // then
+        do {
+            let _ = try result.get()
+            XCTAssert(false)
+        } catch {
+            XCTAssert(true)
+        }
+    }
+
+    private func handleRequest(_ request: URLRequest) -> Result<Data, NetworkError> {
+        if shouldError {
+            return .failure(.missingData)
+        }
+        let imageData = UIImage(named: "warning")?.pngData()
+        return .success(imageData!)
     }
 
 }
